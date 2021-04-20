e86fd08d21e3262e8bb22a179f210c8c43d303c9
Cynthia Anyaeriuba <cynthia.a@Cynthias-MacBook-Pro.local>
2021-04-20 15:24:25 +0100

    Viewing Bookmarks

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-    configure :development do 
-        register Sinatra::reloader
-    end
+	configure :development do 
+		register Sinatra::reloader
+	end
 
-    get '/' do 
-        'Bookmark Manager'
-    end 
+	get '/' do 
+		'Bookmark Manager'
+	end 
 
-    run! if app_file == $0
+	get '/bookmarks' do
+		@bookmarks = Bookmark.all
+		erb :'bookmarks/index'
+	end
+
+	run! if app_file == $0
 end
